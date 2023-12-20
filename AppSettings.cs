98d889322856ebd4221ea3f51846df6a0e98351f
Fabian Dingemans <mail@fabiandingemans.nl>
2023-12-20 11:25:14 +0100

    Created settings page

---
+++ AppSettings.cs
@@ -0,0 +1,18 @@
+namespace FocusTravelApp;
+
+public static class AppSettings
+{
+    
+    public static string BreakReminderInterval
+    {
+        get => Preferences.Default.Get("BREAK_REMINDER_INTERVAL", "30");
+        set => Preferences.Default.Set("BREAK_REMINDER_INTERVAL", value);
+    }
+    
+    public static string DrinkReminderInterval
+    {
+        get => Preferences.Default.Get("DRINK_REMINDER_INTERVAL", "45");
+        set => Preferences.Default.Set("DRINK_REMINDER_INTERVAL", value);
+    }
+    
+}
---
+++ SettingsPage.xaml.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using FocusTravelApp.Managers;
+
+namespace FocusTravelApp;
+
+public partial class SettingsPage : ContentPage
+{
+    private BluetoothManager _bluetoothManager;
+    
+    public SettingsPage()
+    {
+        InitializeComponent();
+        
+        _bluetoothManager = new BluetoothManager();
+
+        BreakReminderEntry.Text = AppSettings.BreakReminderInterval.ToString();
+        DrinkReminderEntry.Text = AppSettings.DrinkReminderInterval.ToString();
+    }
+
+    private void BreakReminder_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        AppSettings.BreakReminderInterval = e.NewTextValue;
+    }
+
+    private void DrinkReminder_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        AppSettings.DrinkReminderInterval = e.NewTextValue;
+    }
+
+    private void SearchDeviceButton_OnClicked(object? sender, EventArgs e)
+    {
+        Debug.WriteLine("SearchDeviceButton_OnClicked");
+        
+        _bluetoothManager.FindDeviceAndConnect(this.UpdateBluetoothStatusText);
+    }
+
+    private void UpdateBluetoothStatusText(string text, Color? color)
+    {
+        Application.Current?.Dispatcher.Dispatch(() =>
+        {
+            BluetoothStatusLabel.Text = text;
+            BluetoothStatusLabel.TextColor = color ?? Color.FromRgb(255, 255, 255);
+        });
+    }
+
+}
