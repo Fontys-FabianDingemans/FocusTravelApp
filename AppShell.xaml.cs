7b956ab23bb05255ca6d3c94f585fc5960f712d5
Sander <sanderwiel2005@gmail.com>
2023-12-21 13:59:32 +0100

    Navigation fixed

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -2,14 +2,36 @@
 
 public partial class AppShell : Shell
 {
+    
+    
+    protected override void OnNavigating(ShellNavigatingEventArgs args)
+    {
+        base.OnNavigating(args);
+
+        if (args.Source == ShellNavigationSource.ShellSectionChanged)
+        {
+            var navigation = Shell.Current.Navigation;
+            var pages = navigation.NavigationStack;
+            for (var i = pages.Count - 1; i >= 1; i--)
+            {
+                navigation.RemovePage(pages[i]);
+            }
+        }
+    }
     public AppShell()
     {
         InitializeComponent();
         Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
         Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
     }
-    private void ProfileButtonClicked(object sender, EventArgs e)
+    
+   
+    private void ProfileButtonClicked(object sender ,EventArgs e)
     {
         Navigation.PopToRootAsync();
     }
+    
+    
 }
+
+    
